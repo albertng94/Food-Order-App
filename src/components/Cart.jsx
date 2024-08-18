1f9df554a14c7c0ebddefa4b508666335ef861d0
albertng94 <albertng94@gmail.com>
2024-08-18 17:19:52 +0200

    Update "Header" and update and implement "Cart".
    Header and Cart components have been updated to be linked by the
    "forwardRef" hook, so that a ref within the Cart "dialog" element can
    be accessed from Header component's "handleOpenCart" function to show
    the modal in the UI.
    
    Header also has been added the "handleCloseModal" function to close the
    modal when the "Close" dialog button is clicked by the user.
    
    Cart component has also been updated to show prices with two decimals at
    most, and to render different content depending on if the user has
    selected items or not.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, forwardRef } from "react";
 import { CartContext } from "../store/shopping-cart-context";
 
 function calculateTotalPrice(itemsArr) {
@@ -9,21 +9,31 @@ function calculateTotalPrice(itemsArr) {
     return totalPrice;
 }
 
-export default function Cart() {
+const Cart = forwardRef(function Cart({handleCloseModal}, ref) {
     
     let { items } = useContext(CartContext);
 
     let totalPrice = calculateTotalPrice(items);
     console.log(totalPrice);
 
+    if (items.length === 0) {
+        return <dialog className="modal" ref={ref}>
+        <h2>Your Cart</h2>
+        <p className="cart-item">Your cart is empty. Make sure to select your favourite meals first.</p>
+        <div className="modal-actions cart-without-items">
+            <button className="button" onClick={handleCloseModal}>Close</button>
+        </div>
+    </dialog>
+    }
+
     return (
-        <dialog className="modal">
+        <dialog className="modal" ref={ref}>
             <h2>Your Cart</h2>
             <ul>
                 {items.map((item) => (
                     <li key={item.id}>
                         <div className="cart-item">
-                            <p>{`${item.name} - ${item.quantity} x $${Number(item.price)*Number(item.quantity)}`}</p>
+                            <p>{`${item.name} - ${item.quantity} x $${(item.price*item.quantity).toFixed(2)}`}</p>
                             <div className="cart-item-actions">
                                 <button>-</button>
                                 <p>{item.quantity}</p>
@@ -33,11 +43,13 @@ export default function Cart() {
                     </li>
                 ))}
             </ul>
-            <p className="cart-total">{`$${totalPrice}`}</p>
-            <div>
-                <button>Close</button>
+            <p className="cart-total">{`$${totalPrice.toFixed(2)}`}</p>
+            <div className="modal-actions">
+                <button className="text-button" onClick={handleCloseModal}>Close</button>
                 <button className="button">Go to checkout</button>
             </div>
         </dialog>
     );
-}
+});
+
+export default Cart;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import logo from "../assets/logo.jpg";
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { CartContext } from "../store/shopping-cart-context";
+import Cart from "./Cart";
 
 export default function Header() {
 
@@ -8,15 +9,28 @@ export default function Header() {
 
     const totalItems = items.length;
 
+    const cartDialog = useRef();
+
+    function handleOpenCart() {
+        cartDialog.current.showModal();
+    }
+
+    function handleCloseModal() {
+        cartDialog.current.close();
+    }
+
     return (
-        <div id="main-header">
-        <div id="title">
-          <img src={logo} alt="A burger logo" />
-          <h1>REACTFOOD</h1>
-        </div>
-        <button className="text-button">
-            {(totalItems > 0) ? `Cart (${totalItems})` : "Cart"}
-        </button>
-      </div>
+        <>
+            <div id="main-header">
+                <div id="title">
+                    <img src={logo} alt="A burger logo" />
+                    <h1>REACTFOOD</h1>
+                </div>
+                <button className="text-button" onClick={handleOpenCart}>
+                    {(totalItems > 0) ? `Cart (${totalItems})` : "Cart"}
+                </button>
+            </div>
+            <Cart ref={cartDialog} handleCloseModal={handleCloseModal} />
+        </>        
     );
 }

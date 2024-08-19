b2fdb10f6cee66e04bda96ae68118a2a26837cbd
albertng94 <albertng94@gmail.com>
2024-08-19 21:06:27 +0200

    Implement update/delete item functionalities within "Cart".
    For the item update functionality:
    
    --> Created "onClick" props within the -/+ button and passed a new
    "updateCartItem" function which is coming from shopping-cart context.
    "-" button uses a conditional statement within the prop so that it only
    executes the updating function when the item quantity is over 1, so that
    the item will have at least "1" of quantity at all times.
    
    --> "updateCartItem" is passed and object with two values, the item id
    and the type of action that the button executes (+ or -) to distinguish
    them.
    
    -->"updateCartItem" connects with the dispatch function
    "handleUpdateCartItem" from the context, which in turn turn triggers the
    "shoppingCartReducer" function. "shoppingCartReducer" detects the action
    type "UPDATE_ITEM" from the dispatch function and increments or
    diminishes the quantity of the specific item by one, depending on the
    action of the button (+/-).
    
    For the item delete functionality:
    
    --> Used a trash "ion-icon" as if it were a button and added some css
    to make it look good.
    
    --> Created an "onClick" prop within the icon and set it to context
    function "deleteCartItem", passing it only the item id as argument.
    
    --> "deleteCartItem" triggers dispatch function "handleDeleteCartItem",
    which trigges "shoppingCartReducer", which detects the "DELETE_ITEM"
    type and filters through the state items array removing the item with
    the same id as the one received.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -11,10 +11,9 @@ function calculateTotalPrice(itemsArr) {
 
 const Cart = forwardRef(function Cart({handleCloseModal}, ref) {
     
-    let { items } = useContext(CartContext);
+    let { items, updateCartItem, deleteCartItem } = useContext(CartContext);
 
     let totalPrice = calculateTotalPrice(items);
-    console.log(totalPrice);
 
     if (items.length === 0) {
         return <dialog className="modal" ref={ref}>
@@ -35,9 +34,17 @@ const Cart = forwardRef(function Cart({handleCloseModal}, ref) {
                         <div className="cart-item">
                             <p>{`${item.name} - ${item.quantity} x $${(item.price*item.quantity).toFixed(2)}`}</p>
                             <div className="cart-item-actions">
-                                <button>-</button>
+                                <button  
+                                    onClick={item.quantity > 1 ? (() => updateCartItem({id: item.id, actionType: "-"})) : undefined}
+                                >-</button>
                                 <p>{item.quantity}</p>
-                                <button>+</button>
+                                <button 
+                                    onClick={() => updateCartItem({id: item.id, actionType: "+"})}    
+                                >+</button>
+                                <ion-icon 
+                                    name="trash-outline"
+                                    onClick={() => deleteCartItem(item.id)}
+                                ></ion-icon>
                             </div>
                         </div>
                     </li>
--- src/store/shopping-cart-context.jsx
+++ src/store/shopping-cart-context.jsx
@@ -37,7 +37,44 @@ function shoppingCartReducer(state, action) {
             return {
                 items: updatedItems
             };
+        }
 
+    } else if (action.type === "UPDATE_ITEM") {
+        const updatedItems = [...state.items];
+
+        const existingCartItemIndex = updatedItems.findIndex(
+            (cartItem) => cartItem.id === action.payload.id
+        );
+    
+        const existingCartItem = updatedItems[existingCartItemIndex];
+ 
+        if (action.payload.actionType === "+") {
+            const updatedItem = {
+                ...existingCartItem,
+                quantity: existingCartItem.quantity + 1,
+            };
+            updatedItems[existingCartItemIndex] = updatedItem;
+
+        } else if (action.payload.actionType === "-") {
+            const updatedItem = {
+                ...existingCartItem,
+                quantity: existingCartItem.quantity - 1,
+            };
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+        
+
+        return {
+            items: updatedItems
+        };
+
+    } else if (action.type === "DELETE_ITEM") {
+        const updatedItems = [...state.items].filter((item) => {
+            return item.id !== action.payload;
+        });
+
+        return {
+            items: updatedItems
         }
     }
 
@@ -63,11 +100,31 @@ export default function CartContextProvider({children}) {
         });
     }
 
+    function handleUpdateCartItem({id, actionType}) {
+        shoppingCartDispatch({
+            type: "UPDATE_ITEM",
+            payload: {
+                id,
+                actionType
+            }
+        });
+    }
+
+    function handleDeleteCartItem(id) {
+        console.log(id);
+        shoppingCartDispatch({
+            type: "DELETE_ITEM",
+            payload: id
+        })
+    }
+
     console.log(shoppingCart.items);;
 
     let cartCtx = {
         items: shoppingCart.items,
         addItemToCart: handleAddItemToCart,
+        updateCartItem: handleUpdateCartItem,
+        deleteCartItem: handleDeleteCartItem
     };
 
     return <CartContext.Provider value={cartCtx}>
